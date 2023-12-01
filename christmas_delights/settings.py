b68520c32b637b52b9a5434dc7fa87e6f056739b
AstaJoks <>
2023-12-01 23:22:34 +0000

    Set debug to false

--- christmas_delights/settings.py
+++ christmas_delights/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = FalseS
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-astajoks-christmas-delig-8oy7z8kwji.us2.codeanyapp.com',
