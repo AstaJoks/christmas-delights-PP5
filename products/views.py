1f2e9fabaeb77fb3c527385de3738c1289a21927
AstaJoks <>
2023-12-01 11:28:34 +0000

    Fixed the image field

--- products/views.py
+++ products/views.py
@@ -31,7 +31,7 @@ def all_products(request):
 
             if sortkey == 'category':
                 sortkey = 'category__name'
-               
+
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -148,7 +148,7 @@ def delete_product(request, product_id):
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
-        
+
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
     messages.success(request, 'Product deleted!')
