0a272c71dbbe5ba1345e16a90f042ea9664e602a
AstaJoks <>
2023-12-02 15:14:28 +0000

    Added the model, view and templates for contact form

--- christmas_delights/settings.py
+++ christmas_delights/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-astajoks-christmas-delig-8oy7z8kwji.us2.codeanyapp.com',
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+    'contact',
 
     # Other
     'crispy_forms',
---
+++ contact/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from .forms import ContactUsForm
+# Create your views here.
+
+
+def contact_us(request):
+    """ View function for the contact us form """
+
+    if request.method == 'POST':
+        form = ContactUsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_success')
+    else:
+        form = ContactUsForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'contact/contact.html', context)
+
+
+def contact_success(request):
+    """ Render the Contact Success HTML page """
+
+    return render(request, 'contact/contact_success.html')
