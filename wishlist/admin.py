9fb78c19bddbe3f67d96b1af278a26c0d4a212d0
AstaJoks <>
2023-12-02 20:08:15 +0000

    Fixed margins for mobile

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Wishlist
 
 # Register your models here.
+
+
+class WishlistAdmin(admin.ModelAdmin):
+    # search by username
+    search_fields = [
+        "user",
+    ]
+    # adds boxes side by side to allow removing/adding products in admin
+    filter_horizontal = ("products",)
+    list_display = (
+        "user",
+        "date_added",
+    )
+
+    ordering = ("user",)
+
+
+admin.site.register(Wishlist, WishlistAdmin)
