5ceda83b236331d85b7da4fc9dbdc4b9f729b8f4
AstaJoks <>
2023-12-03 15:57:14 +0000

    Added the error pages

--- christmas_delights/urls.py
+++ christmas_delights/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
+from .views import handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +32,6 @@ urlpatterns = [
     path('wishlist/', include('wishlist.urls')),
     path("faqs/", include("faqs.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'christmas_delights.views.handler404'
+handler404 = 'christmas_delights.views.handler500'
