3b1f95a8c98099d806404634e2f2fae3fa14d590
AstaJoks <>
2023-12-03 16:41:41 +0000

    Added 403 error template

--- christmas_delights/views.py
+++ christmas_delights/views.py
@@ -6,6 +6,11 @@ def handler404(request, exception):
     return render(request, "errors/404.html", status=404)
 
 
+def handler403(request, exception):
+    """ Error Handler 403 """
+    return render(request, "errors/403.html", status=403)
+
+
 def handler500(request, exception):
     """
     Displays a custom 500 error page.
