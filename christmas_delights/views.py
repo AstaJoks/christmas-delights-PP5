0b134f06c1cc5ad575d59033080ec4689f6d09c9
AstaJoks <>
2023-12-03 16:27:12 +0000

    Fixed the erroe pages

--- christmas_delights/views.py
+++ christmas_delights/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
     return render(request, "errors/404.html", status=404)
+
+
+def handler500(request, exception):
+    """
+    Displays a custom 500 error page.
+    """
+    return render(request, 'errors/500.html', status=500)
